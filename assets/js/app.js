16cc15a6bb28b5c288c697b46e5135dfe42cdf2f
EmranCo <alhaddademran@gmail.com>
2022-01-18 23:47:28 +0300

    add signup popup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,2 @@
-const showModula = id => document.getElementById(id).style.visibility = "visible";
+const showModule = id => document.getElementById(id).style.visibility = "visible";
 const hideModule = id => document.getElementById(id).style.visibility = "hidden";
